083c9090427d04bcece4c36b31e4e91c05565658
Tho Nguyen (Todd) <todd.nguyen47@gmail.com>
2019-01-25 17:05:06 -0800

    Added an exclude and include button

--- RandomCiv/Form1.Designer.cs
+++ RandomCiv/Form1.Designer.cs
@@ -28,40 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.randomizeButton = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.excludeButton = new System.Windows.Forms.Button();
+            this.includeButton = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.AutoSize = true;
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.listBox1, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
-            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(-3, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 455F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(798, 466);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 24;
-            this.listBox1.Location = new System.Drawing.Point(402, 3);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(393, 460);
-            this.listBox1.TabIndex = 0;
             // 
             // flowLayoutPanel1
             // 
@@ -95,17 +76,101 @@
             this.randomizeButton.UseVisualStyleBackColor = true;
             this.randomizeButton.Click += new System.EventHandler(this.randomizeButton_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(-3, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 455F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(798, 755);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.listBox1);
+            this.flowLayoutPanel2.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel2.Controls.Add(this.listBox2);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(402, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(389, 697);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(393, 316);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 1;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.flowLayoutPanel3.AutoSize = true;
+            this.flowLayoutPanel3.Controls.Add(this.excludeButton);
+            this.flowLayoutPanel3.Controls.Add(this.includeButton);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(50, 325);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(298, 46);
+            this.flowLayoutPanel3.TabIndex = 4;
+            // 
+            // excludeButton
+            // 
+            this.excludeButton.Location = new System.Drawing.Point(3, 3);
+            this.excludeButton.Name = "excludeButton";
+            this.excludeButton.Size = new System.Drawing.Size(143, 40);
+            this.excludeButton.TabIndex = 3;
+            this.excludeButton.Text = "Exclude ↓";
+            this.excludeButton.UseVisualStyleBackColor = true;
+            this.excludeButton.Click += new System.EventHandler(this.excludeButton_Click);
+            // 
+            // includeButton
+            // 
+            this.includeButton.Location = new System.Drawing.Point(152, 3);
+            this.includeButton.Name = "includeButton";
+            this.includeButton.Size = new System.Drawing.Size(143, 40);
+            this.includeButton.TabIndex = 4;
+            this.includeButton.Text = "Include ↑";
+            this.includeButton.UseVisualStyleBackColor = true;
+            this.includeButton.Click += new System.EventHandler(this.includeButton_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 24;
+            this.listBox2.Location = new System.Drawing.Point(3, 377);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(393, 316);
+            this.listBox2.Sorted = true;
+            this.listBox2.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 709);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Random AoE2 Expansion Civizalition";
-            this.tableLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,18 +187,22 @@
                 this.listBox1.Items.Add(s);
             }
         }
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button randomizeButton;
         private readonly System.Random m_random = new System.Random();
         private string[] listOfExpansionCivs = {"Berbers (Camel Archer)","Burmese (Arambai)",
             "Ethiopians (Shotel Warrior)", "Incas (Kamayuk)", "Indians (Elephant Archer)",
-            "Italians (Genoese Crossbowman)", "Magyars (Magyar Huszar)",
+            "Italians (Genoese Crossbowman)", "Khmer (Ballista Elephant)", "Magyars (Magyar Huszar)",
             "Malay (Karambit Warrior)", "Malians (Gbeto)", "Portuguese (Organ Gun)",
             "Slavs (Boyar)", "Vietnamese (Rattan Archer)"};
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button randomizeButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button excludeButton;
+        private System.Windows.Forms.Button includeButton;
     }
 }
 
--- RandomCiv/Form1.cs
+++ RandomCiv/Form1.cs
@@ -20,10 +20,55 @@ namespace RandomCiv
 
         private void randomizeButton_Click(object sender, EventArgs e)
         {
-            int totalCivCount = this.listOfExpansionCivs.Length;
+            // Get the total count of the desired civs to randomize from
+            int totalCivCount = this.listBox1.Items.Count;
             int randomIndex = this.m_random.Next(0, totalCivCount);
 
-            this.textBox1.Text = this.listOfExpansionCivs[randomIndex];
+            this.textBox1.Text = this.listBox1.Items[randomIndex].ToString();
+        }
+
+
+        /** <summary>
+         * Remove the item at index from listboxInput and add that item to listboxOutput.
+         * </summary>
+         * */
+        private void RemoveAndAdd(int index, ListBox listboxInput, ListBox listboxOutput)
+        {
+            // Check if index is null
+            if (index >= 0)
+            {
+                Object selectedItem = listboxInput.Items[index];
+                // Remove the item from the listbox input
+                listboxInput.Items.RemoveAt(index);
+                // Add the item to listbox output 
+                listboxOutput.Items.Add(selectedItem);
+            }
+        }
+
+
+        private void excludeButton_Click(object sender, EventArgs e)
+        {
+            // Get the selected index of the selected value in listBox1
+            int indexOfSelectedCiv = this.listBox1.SelectedIndex;
+
+            // If there is a selected civ, i.e. it is not null then the index will not be negative 1
+            if (indexOfSelectedCiv >= 0)
+            {
+                this.RemoveAndAdd(indexOfSelectedCiv, this.listBox1, this.listBox2);
+            }
+        }
+
+
+        private void includeButton_Click(object sender, EventArgs e)
+        {
+            // Get the selected index of the selected value in listBox1
+            int indexOfSelectedCiv = this.listBox2.SelectedIndex;
+
+            // If there is a selected civ, i.e. it is not null then the index will not be negative 1
+            if (indexOfSelectedCiv >= 0)
+            {
+                this.RemoveAndAdd(indexOfSelectedCiv, this.listBox2, this.listBox1);
+            }
         }
     }
 }
